6919d756fa8accf617a04f6562c50ec4cb5a316b
lVladimirl <infinitdungeun@gmail.com>
2022-11-06 23:52:58 -0300

    Initial commit

---
+++ src/components/header/index.jsx
@@ -0,0 +1,12 @@
+import './index.css'
+export const Header = () => {
+    return (
+        <header className="Header">
+            {/* <img src="logo"alt="Nu Kenzie" /> */}
+            <h1>titulo</h1>
+            <button>Inicio</button>
+        </header>
+    )  
+  }
+
+export default Header
